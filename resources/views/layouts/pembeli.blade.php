b5e2d34ee21d0364db623db9e00ad57d455edac5
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-03-27 08:59:26 +0700

    update tailwind

---
+++ resources/views/layouts/pembeli.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'CoE Treemine')</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+
+<body>
+
+    <header>
+        @include('components.sidebar')
+    </header>
+    <div class="min-h-100">
+        <h1 class="text-xl font-bold dark:text-white">List Produk</h1>
+        <div class="container">
+            <main>
+                @yield('content')
+            </main>
+        </div>
+    </div>
+    <!-- <footer>
+        @include('components.footer')
+    </footer> -->
+</body>
+
+</html>
