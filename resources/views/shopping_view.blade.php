94eecce96cf1885bacc21eb2d473bfc284cbd4bb
reezqiii <velasyifa02@gmail.com>
2024-03-31 00:05:32 +0700

    update shopping cart custom

--- resources/views/shopping_view.blade.php
+++ resources/views/shopping_view.blade.php
@@ -10,7 +10,6 @@
 </head>
 
 <body>
-
     <nav class="border-gray-200 bg-green-950 dark:border-gray-700">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -19,6 +18,7 @@
         </div>
     </nav>
 
+    </div>
     <div class="mt-10 flex justify-center gap-24">
         <button class="bg-gray-300 hover:bg-gray-800 hover:text-white font-bold py-2 px-10 rounded">
             Custom
@@ -27,65 +27,90 @@
             Custom
         </button>
     </div>
-
-    <div>
         <div>
-
-            <div class="inline-grid grid-cols-6">
+            <div class="inline-grid grid-cols-6 ml-12 mt-8 my-5">
                 <div class="inline-grid grid-cols-2">
                     <input type="checkbox" name="" id="">
-                    <h1>ALL</h1>
+                    <h1>All</h1>
                 </div>
                 <div>
-                  <h1>Product Name & Details</h1>
+                    <h1>Product Name & Details</h1>
+                </div>
+                <div class="ml-10">
+                    <h1>Quantity</h1>
                 </div>
                 <div>
-                  <h1>Quantity</h1>
+                    <h1>Price</h1>
                 </div>
                 <div>
-                  <h1>Price</h1>
+                    <h1>Product File</h1>
                 </div>
                 <div>
-                  <h1>Product File</h1>
-                </div>
-                <div>
-                  <h1>Status</h1>
+                    <h1>Status</h1>
                 </div>
             </div>
-            <div class="inline-grid grid-cols-6">
-                <div>
-                  <input type="checkbox" name="" id="">
-                </div>
-                <div>
-                    <img src="./Assets/PCB 1/Complete.jpg" alt="">
-                    <h1>2 Layers Size 10x10mm 1.6mm</h1>
-                    <p>Product No : WWR45TYFVG4</p>
-                    <p>Solder Mask: Green</p>
-                    <p>Silkscreen : White</p>
-                    <h2>View Detail</h2>
-                </div>
-                <div>
-                  <input type="number" name="" id="">
-                </div>
-                <div>
-                  <h1>Rp.200.00</h1>
-                  <p>(0,01 kg)</p>
-                </div>
-                <div>
-                  <input type="file" name="" id="">
-                </div>
-                <div>
-                  <input type="radio" name="" id="">
-                  <h1>Not Completed</h1>
-                </div>
-            </div>
+            <div class="bg-gray-100 ml-10  mr-10 shadow-md p-0">
+                            <div class="flex items-center">
+            
+                    <div class="inline-grid grid-cols-9">
+                        <div>
+                            <input type="checkbox" name="" id="">
+                        </div>
+                        
+                <img src="./image/development board.jpg" alt="Image" class="ml-10" >
+          <div class="ml-10">
+                            <p>2 Layers Size 10x10mm 1.6mm</p>
+                            <p>Product No : WWR45TYFVG4</p>
+                            <p>Solder Mask: Green</p>
+                            <p>Silkscreen : White</p>
+                            <h2>View Detail</h2>
+                        </div>
+                        <div>
+                            <input type="number" name="" id="">
+                        </div>
+                        <div>
+                            <h1>Rp.200.00</h1>
+                            <p>(0,01 kg)</p>
+                        </div>
+                        <div>
+                            <input type="file" name="" id="">
+                        </div>
+                        <div>
+                            <input type="radio" name="" id="">
+                            <h1>Not Completed</h1>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
 
-        </div>
-    </div>
+                    <div>
+                    <div class="bg-gray-200 mt-10 ml-10 mr-10 shadow-md ">
+                            <div class="flex items-center">
+                      
+                                <input id="update-check" type="checkbox"
+                                    class="h-4 w-4 text-blue-600  focus:ring-blue-500 border-gray-300 rounded">
+                                <label for="update-check" class="ml-2 block text-sm text-gray-800 ">Check All</label>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div class="grid grid-cols-3">
+                            <div>
+                                <div class="text-sm text-gray-600 mb-10 my-10">
+                                    SubTotal (1 items) : <span class="font-bold">Rp.400.000</span> <br>
+                                    All total : <span class="text-black font-bold">Rp.400.000</span>
+                                </div>
+                                <div>
+                                    <div class="mt-4 mb-10 ">
+                                        <button
+                                            class="bg-green-300 hover:bg-green-800 hover:text-white text-black font-bold py-2 px-4 rounded">
+                                            Proceed to checkout
+                                        </button>
+                                    </div>
 
 
 
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js">
-        < /body> <
-        /html>
+                                    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js">
+                                        < /body> 
+                                        </html>
