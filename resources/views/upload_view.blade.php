1a033d17c3955aaaf3c1d87f3f463e244bbe819b
reezqiii <velasyifa02@gmail.com>
2024-04-02 22:53:14 +0700

    update: upload file

--- resources/views/upload_view.blade.php
+++ resources/views/upload_view.blade.php
@@ -27,70 +27,46 @@
         </div>
     </nav>
     <div class="bg-white shadow-md p-4 rounded-md">
-      
     <div class="ml-8 my-5 text-2xl font-bold text-gray-800">
   <span class="text-black">Attach Your Gerber Files</span>
   <p class="text-base inline font-medium text-green-800 mx-5">PCBway will never disclose your PCB files to any third party!</p>
 </div>
-<tbody>
-    <tr>
-      <td class="border-t px-6 py-4">
-        <div class="flex items-center">
-        <input type="checkbox" name="" id="" class="ml-10">
-          <div class="w-6 h-6 mr-2">
-          </div>
-          <div class="font-medium text-gray-800">Gerber Files</div>
-        </div>
-      </td>
-  <td class=" border-t px-6 py-4">
-        <div class="flex items-center">
-        <input type="checkbox" name="" id="" class="ml-10">
-          <div class="w-6 h-6 mr-2">
-          </div>
-          <div class="font-medium text-gray-800">DXP</div>
-        </div>
-      </td>
-      <td class="border-t px-6 py-4">
-        <div class="flex items-center">
-        <input type="checkbox" name="" id="">
-          <div class="w-6 h-6 mr-2">
-          </div>
-          <div class="font-medium text-gray-800">Others</div>
-        </div>
-      </td>
+<div class="flex items-center mt-10">
+  <input id="checkbox1" type="checkbox" class="ml-8 form-checkbox h-5 w-5 text-blue-600">
+  <label for="checkbox1" class="ml-5">Gerber File</label>
+  <div class="flex-1"></div>
+  <input id="checkbox2" type="checkbox" class="ml-auto form-checkbox h-5 w-5 text-blue-600 ">
+  <label for="checkbox2" class="ml-5">DXP</label>
+  <div class="flex-1"></div>
+  <input id="checkbox3" type="checkbox" class="mr-8 form-checkbox h-5 w-5 text-blue-600">
+  <label for="checkbox3" class="mr-40">Other</label>
 </div>
-
-      <div >
+</div>
+</div>
+      <div class="flex flex-col items-center bg-white p-6 rounded-lg shadow-md" >
       <td class="border-t px-6 py-4">
-        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+        <button class="mt-10 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
           + Add Gerber File
         </button>
+        <span class="mt-9 text-gray-500 ml-2">.rar, .zip or Maximum 50M (All uploads are secure and confidential.)</span>
+        </div>
         </div>
       </td>
     </tr>
-  <div class="mt-2">
-    
-    <span class="text-gray-500 ml-2">.rar, .zip or Maximum 50M</span>
-  </div>
-  
  
-     
-      
 
-     
-    <tr>
-     
-  </tbody>
-</table>
-
-  <div class="mt-10 ml-5">
+<div class="flex mt-10 ml-5 ">
+  <div>
     <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
       Submit Order Now
     </button>
   </div>
-  <div class="mt-10 ml-5">
-    <button class="bg-gray-400 text-gray-800 hover:underline py-1 px-2 ">skip</button>
+  <div class="ml-4 mt-auto">
+    <button class="bg-gray-400 text-gray-800 hover:underline py-2 px-2 ">
+      skip
+    </button>
   </div>
+</div>
 </div>
 </body>
 </html>
