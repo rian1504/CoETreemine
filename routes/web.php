69ffb902328b877d4fe01820c9d90727980aaadf
alealily <alialily782@gmail.com>
2024-04-20 22:18:06 +0700

    fix route web

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,65 @@
 <?php
 
-use App\Http\Controllers\DetailController;
-use App\Http\Controllers\HistoryController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PortofolioController;
-use App\Http\Controllers\ProductKategoriController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\EmailController;
-use App\Http\Controllers\PasswordController;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ShoppingController;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\CardetailController;
-
 
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', [LoginController::class, 'login']);
-Route::get('/register', [RegisterController::class, 'register']);
-Route::get('/email', [EmailController::class, 'email']);
-Route::get('/password', [PasswordController::class, 'password']);
-Route::get('/home', [HomeController::class, 'home']);
-Route::get('/product', [ProductKategoriController::class, 'product']);
-Route::get('/service', [ServiceController::class, 'service']);
-Route::get('/portofolio', [PortofolioController::class, 'portofolio']);
-Route::get('/detail', [DetailController::class, 'detail']);
-Route::get('/payment', [PaymentController::class, 'payment']);
-Route::get('/shopping', [ShoppingController::class, 'shopping']);
-Route::get('/file', [UploadController::class, 'file']);
-Route::get('/cardcust', [CardetailController::class, 'cardcust']);
-Route::get('/upload', [UploadController::class, 'upload']);
-Route::get('/history', [HistoryController::class, 'history']);
+
+Route::get('/email', function () {
+    return view('email');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+Route::get('/password', function () {
+    return view('password');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/product', function () {
+    return view('product_kategori');
+});
+
+Route::get('/portofolio', function () {
+    return view('portofolio');
+});
+
+Route::get('/detail', function () {
+    return view('detail_portofolio');
+});
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/payment', function () {
+    return view('payment');
+});
+
+Route::get('/shopping', function () {
+    return view('shopping');
+});
+
+Route::get('/history', function () {
+    return view('history');
+});
+
+Route::get('/upload', function () {
+    return view('upload');
+});
+
+Route::get('/cardetail', function () {
+    return view('cardetail');
+});
+
 
