776ae8ead34e6baf4335a529cca0e7b9cb887515
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-04-23 21:55:26 +0700

    ADD navbar for buyer

--- resources/views/layouts/pembeli.blade.php
+++ resources/views/layouts/pembeli.blade.php
@@ -11,15 +11,15 @@
 <body>
 
     <header>
-        @include('components.sidebar')
+        @include('components.header')
     </header>
-    <div class="min-h-100">
-        <h1 class="text-xl font-bold dark:text-white">List Produk</h1>
-        <div class="container">
-            <main>
-                @yield('content')
-            </main>
-        </div>
+    <!-- <div class="min-h-100"> -->
+    <!-- <h1 class="text-xl font-bold dark:text-white">List Produk</h1> -->
+    <div class="container">
+        <main>
+            @yield('content')
+        </main>
+    </div>
     </div>
     <!-- <footer>
         @include('components.footer')
--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,46 @@
 <?php
 
-use App\Http\Controllers\DetailController;
-use App\Http\Controllers\HistoryController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PortofolioController;
-use App\Http\Controllers\ProductKategoriController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\EmailController;
-use App\Http\Controllers\PasswordController;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\ShoppingController;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\CardetailController;
+use Illuminate\Support\Facades\Route;
+
+// use App\Http\Controllers\DetailController;
+// use App\Http\Controllers\HistoryController;
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\PortofolioController;
+// use App\Http\Controllers\ProductKategoriController;
+// use App\Http\Controllers\LoginController;
+// use App\Http\Controllers\RegisterController;
+// use App\Http\Controllers\EmailController;
+// use App\Http\Controllers\PasswordController;
+// use App\Http\Controllers\ServiceController;
+// use App\Http\Controllers\PaymentController;
+// use App\Http\Controllers\ShoppingController;
+// use App\Http\Controllers\UploadController;
+// use App\Http\Controllers\CardetailController;
 
 
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/login', [LoginController::class, 'login']);
-Route::get('/register', [RegisterController::class, 'register']);
-Route::get('/email', [EmailController::class, 'email']);
-Route::get('/password', [PasswordController::class, 'password']);
-Route::get('/home', [HomeController::class, 'home']);
-Route::get('/product', [ProductKategoriController::class, 'product']);
-Route::get('/service', [ServiceController::class, 'service']);
-Route::get('/portofolio', [PortofolioController::class, 'portofolio']);
-Route::get('/detail', [DetailController::class, 'detail']);
-Route::get('/payment', [PaymentController::class, 'payment']);
-Route::get('/shopping', [ShoppingController::class, 'shopping']);
-<<<<<<< HEAD
-Route::get('/file', [UploadController::class, 'file']);
-Route::get('/cardcust', [CardetailController::class, 'cardcust']);
-=======
-Route::get('/upload', [UploadController::class, 'upload']);
-Route::get('/history', [HistoryController::class, 'history']);
->>>>>>> ccf60f40a4e8cf11a74132f9ea7853fba1d0652c
+// Route::get('/login', [LoginController::class, 'login']);
+// Route::get('/register', [RegisterController::class, 'register']);
+// Route::get('/email', [EmailController::class, 'email']);
+// Route::get('/password', [PasswordController::class, 'password']);
+// Route::get('/home', [HomeController::class, 'home']);
+// Route::get('/product', [ProductKategoriController::class, 'product']);
+// Route::get('/service', [ServiceController::class, 'service']);
+// Route::get('/portofolio', [PortofolioController::class, 'portofolio']);
+// Route::get('/detail', [DetailController::class, 'detail']);
+// Route::get('/payment', [PaymentController::class, 'payment']);
+// Route::get('/shopping', [ShoppingController::class, 'shopping']);
+// Route::get('/file', [UploadController::class, 'file']);
+// Route::get('/cardcust', [CardetailController::class, 'cardcust']);
+// Route::get('/upload', [UploadController::class, 'upload']);
+// Route::get('/history', [HistoryController::class, 'history']);
 
+Route::get('/home', function () {
+    return view('layouts.pembeli');
+});
+Route::get('/halaman', function () {
+    return view('layouts.pembeli');
+});
