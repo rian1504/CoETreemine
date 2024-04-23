bbd892f57c507a45492d9313447b038d2091c870
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-04-23 23:24:21 +0700

    Fix colors customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,23 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
-        "./node_modules/flowbite/**/*.js"
+        "./node_modules/flowbite/**/*.js",
     ],
     theme: {
         extend: {},
         colors: {
-            transparent: 'F5F5F5',
-            grey: 'D9D9D9',
-            primary: '047C1E',
-            secondary: '618274',
-            tertiery: 'B0D9B1',
-            quaternary: '79AC78'
-        }
+            greys: {
+                700: "#EEF0E5",
+                800: "#F5F5F5",
+                900: "#D9D9D9"
+            },
+            greens: {
+                600: "#B0D9B1",
+                700: "#79AC78",
+                800: "#618274",
+                900: "#047C1E",
+            }
+        },
     },
-    plugins: [
-        require('flowbite/plugin')]
+    plugins: [require("flowbite/plugin")],
 };
