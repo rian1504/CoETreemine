d58378acb9b00fa44f643d84daef04c947f72dac
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-04-24 11:19:36 +0700

    Fix buyer header and guest header

--- resources/views/components/guest_header.blade.php
+++ resources/views/components/guest_header.blade.php
@@ -14,10 +14,10 @@
             </div>
             <div class="flex w-1/2 justify-end gap-4">
                 <div class="w-16">
-                    <a href="/custom_cart" class="mr-1">
+                    <a href="/login" class="mr-1">
                         <i class="fas fa-cart-shopping fa-lg"></i>
                     </a>
-                    <a href="/history" class="ml-1">
+                    <a href="/login" class="ml-1">
                         <i class="fas fa-history fa-lg"></i>
                     </a>
                 </div>
@@ -42,16 +42,16 @@
     <div class="bg-greens-900 h-16 w-full id=" navbar-user">
         <ul class="flex flex-col ml-40 text-lg text-white font-medium p-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-                <a href="#" class="block py-2 px-3 md:bg-transparent md:p-0 md:dark:text-blue-500 hover:text-black underline" aria-current="page">Home</a>
+                <a href="/guest" class="block py-2 px-3 md:bg-transparent md:p-0 md:dark:text-blue-500 hover:text-black underline" aria-current="page">Home</a>
             </li>
             <li>
-                <a href="#" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Custom</a>
+                <a href="/custom" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Custom</a>
             </li>
             <li>
-                <a href="#" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Product</a>
+                <a href="portfolio" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Product</a>
             </li>
             <li>
-                <a href="#" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                <a href="service" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
             </li>
         </ul>
     </div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -13,17 +13,17 @@
                 </div>
             </div>
             <div class="flex w-1/2 justify-end">
-                <div class="w-16">
-                    <a href="/custom_cart" class="mr-1">
+                <div class="flex w-16 gap-2">
+                    <a href="/custom_cart">
                         <i class="fas fa-cart-shopping fa-lg"></i>
                     </a>
-                    <a href="/history" class="ml-1">
+                    <a href="/history">
                         <i class="fas fa-history fa-lg"></i>
                     </a>
                 </div>
                 <div class="flex">
                     <div class="mx-2">
-                        <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <div class="flex gap-2 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
                             <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                                 <span class="sr-only">Open user menu</span>
                                 <img class="w-8 h-8 rounded-full" src="image/user.jpeg" alt="user photo">
@@ -74,16 +74,16 @@
     <div class="bg-greens-900 h-16 w-full id=" navbar-user">
         <ul class="flex flex-col ml-40 text-lg text-white font-medium p-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-                <a href="#" class="block py-2 px-3 md:bg-transparent md:p-0 md:dark:text-blue-500 hover:text-black underline" aria-current="page">Home</a>
+                <a href="/buyer" class="block py-2 px-3 md:bg-transparent md:p-0 md:dark:text-blue-500 hover:text-black underline" aria-current="page">Home</a>
             </li>
             <li>
-                <a href="#" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Custom</a>
+                <a href="/custom" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Custom</a>
             </li>
             <li>
-                <a href="#" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Product</a>
+                <a href="/portfolio" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Product</a>
             </li>
             <li>
-                <a href="#" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+                <a href="service" class="block py-2 px-3 hover:text-black md:hover:bg-transparent md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
             </li>
         </ul>
     </div>
