21132b5a955e59709e2e5e73ea2ac3e65776ee09
reezqiii <velasyifa02@gmail.com>
2024-05-11 12:27:04 +0700

    view:buyer card_detail

--- resources/views/buyer/detail_custom_cart.blade.php
+++ resources/views/buyer/detail_custom_cart.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Custom Cart</title>
+    <title>CARD DETAIL CUSTOMER</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::get('/verify', function () {
 });
 
 
+
 // buyer
 
 Route::get('/buyer', function () {
