5eddb312e45df348fc047450c172acc7207e8ac2
reezqiii <velasyifa02@gmail.com>
2024-05-11 11:58:54 +0700

    view : carddetail costum

--- resources/views/cardetail.blade.php
+++ resources/views/cardetail.blade.php
@@ -7,40 +7,135 @@
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
-<nav class="bg-gray-300 dark:border-gray-700">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <span class="ml-5 self-center text-5 font-medium text-black">Product Detail</span>
+<nav class="bg-gray-200 dark:border-gray-700">
+        <div class="flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="#" class="flex items-center ">
+                <span class="ml-20 self-center text-5 font-medium text-black">Product Detail</span>
             </a>
         </div>
     </nav>
     <div class="ml-20 my-10 font-bold text-gray-800">
-  <span class="text-black">Basic Information : Standard PCB</span>
+  <span class="text-black text-lg">Basic Information : Standard PCB</span>
 </div>
-
-<div class="grid grid-cols-2 gap-4">
-  <table class="table-auto w-1/2">
+<div class="grid grid-cols-2 gap-4 ">
+  <table class="ml-20 table-auto">
     <tbody>
       <tr>
-        <td class="border border-gray-300 px-4 py-2">Product No :</td>
-        <td class="border border-gray-300 px-4 py-2"></td>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300 px-4 py-2">Product No :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2">3312301013 </td>
       </tr>
       <tr>
-        <td class="border border-gray-300 px-4 py-2">Create Time :</td>
-        <td class="border border-gray-300 px-4 py-2"></td>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Create Time :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
       </tr>
       <tr>
-        <td class="border border-gray-300 px-4 py-2">Estimated Finish Time :</td>
-        <td class="border border-gray-300 px-4 py-2"></td>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Estimated Finish Time :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
       </tr>
       <tr>
-        <td class="border border-gray-300 px-4 py-2">SMT for PCB Service NO</td>
-        <td class="border border-gray-300 px-4 py-2"></td>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">SMT for PCB Service NO</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
       </tr>
+    </tbody>
+    <table class="ml-20 table-auto ">
+    <tbody>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300 px-4 py-2">Order Status  :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2">3312301013 </td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Build Time :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Gerber File :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      
     </tbody>
   </table>
 </div>
+<div class="ml-20 my-10 font-bold text-gray-800">
+  <span class="text-black text-lg">Basic Information : Standard PCB</span>
     </div>
+    <div class="grid grid-cols-2 gap-4 ">
+  <table class="ml-20 table-auto">
+    <tbody>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300 px-4 py-2">Board Type :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2">Single Piece </td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Different design in panel :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Size:</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Layer :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Layer :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Min Hole Size :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Silkscreen :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Surface Finish :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Via Process :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">PO No.</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <table class="ml-20 table-auto">
+    <tbody>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300 px-4 py-2">Panel Way ::</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2">Single Piece </td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">X- out Allowance in panel :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Quantity :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Material :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Min track/spacing :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Solder Mask :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class=" border-gray-400 border-2 font-semibold bg-gray-300  px-4 py-2">Manufacturing :</td>
+        <td class=" border-gray-400 border-2 bg-gray-200 px-4 py-2"></td>
+      </tr>
+      
+    </tbody>
+    
+  </table>
+</div>
     </div>
 </body>
 </html>
