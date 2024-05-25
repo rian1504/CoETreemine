1ba94932010dca2ab785f62faaf29083f9d863aa
rian1504 <adaadi3526@gmail.com>
2024-05-25 16:47:03 +0700

    Feat: CRUD Prototype Inner Cooper Membuat CRUD pada Prototype Inner Cooper

--- app/Http/Controllers/Admin/PrototypeInnerCooperController.php
+++ app/Http/Controllers/Admin/PrototypeInnerCooperController.php
@@ -14,7 +14,10 @@ class PrototypeInnerCooperController extends Controller
      */
     public function index()
     {
-        //
+        //redirect to index
+        return redirect()->route('prototype.index')->with([
+            'select' => 'Inner Cooper'
+        ]);
     }
 
     /**
@@ -22,7 +25,7 @@ class PrototypeInnerCooperController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.prototype.inner_cooper.create');
     }
 
     /**
@@ -30,38 +33,90 @@ class PrototypeInnerCooperController extends Controller
      */
     public function store(StorePrototypeInnerCooperRequest $request)
     {
-        //
+        //validate form
+        $request->validate([
+            'inner_cooper_name' => 'required|min:5',
+            'inner_cooper_price' => 'required|numeric'
+        ]);
+
+        //create Inner Cooper
+        PrototypeInnerCooper::create([
+            'inner_cooper_name' => $request->inner_cooper_name,
+            'inner_cooper_price' => $request->inner_cooper_price
+        ]);
+
+        //redirect to index
+        return redirect()->route('prototype.index')->with([
+            'success' => 'Data Berhasil Disimpan!',
+            'select' => 'Inner Cooper'
+        ]);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(PrototypeInnerCooper $prototypeInnerCooper)
+    public function show(PrototypeInnerCooper $inner_cooper)
     {
-        //
+        //redirect to index
+        return redirect()->route('prototype.index')->with([
+            'select' => 'Inner Cooper'
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(PrototypeInnerCooper $prototypeInnerCooper)
+    public function edit(PrototypeInnerCooper $inner_cooper)
     {
-        //
+        //get inner_cooper by ID
+        $inner_cooper = PrototypeInnerCooper::findOrFail($inner_cooper->id_inner_cooper);
+
+        //render view with inner_cooper
+        return view('admin.prototype.inner_cooper.edit', compact('inner_cooper'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePrototypeInnerCooperRequest $request, PrototypeInnerCooper $prototypeInnerCooper)
+    public function update(UpdatePrototypeInnerCooperRequest $request, PrototypeInnerCooper $inner_cooper)
     {
-        //
+        //validate form
+        $request->validate([
+            'inner_cooper_name' => 'required|min:5',
+            'inner_cooper_price' => 'required|numeric'
+        ]);
+
+        //get inner_cooper by ID
+        $inner_cooper = PrototypeInnerCooper::findOrFail($inner_cooper->id_inner_cooper);
+
+        // update inner_cooper
+        $inner_cooper->update([
+            'inner_cooper_name' => $request->inner_cooper_name,
+            'inner_cooper_price' => $request->inner_cooper_price
+        ]);
+
+        //redirect to index
+        return redirect()->route('prototype.index')->with([
+            'success' => 'Data Berhasil Diubah!',
+            'select' => 'Inner Cooper'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(PrototypeInnerCooper $prototypeInnerCooper)
+    public function destroy(PrototypeInnerCooper $inner_cooper)
     {
-        //
+        //get inner_cooper by ID
+        $inner_cooper = PrototypeInnerCooper::findOrFail($inner_cooper->id_inner_cooper);
+
+        //delete inner_cooper
+        $inner_cooper->delete();
+
+        //redirect to index
+        return redirect()->route('prototype.index')->with([
+            'success' => 'Data Berhasil Dihapus!',
+            'select' => 'Inner Cooper'
+        ]);
     }
 }
--- resources/views/admin/prototype/inner_cooper/edit.blade.php
+++ resources/views/admin/prototype/inner_cooper/edit.blade.php
@@ -1,14 +1,16 @@
 <h1>Edit</h1>
 
-<form action="{{ route('side.update', $side->id_side) }}" method="POST">
+<form action="{{ route('inner_cooper.update', $inner_cooper->id_inner_cooper) }}" method="POST">
     @csrf
     @method('PUT')
 
     <label for="">Name</label>
-    <input type="text" name="side_name" value="{{ old('side_name', $side->side_name) }}">
+    <input type="text" name="inner_cooper_name"
+        value="{{ old('inner_cooper_name', $inner_cooper->inner_cooper_name) }}">
 
     <label for="">Price</label>
-    <input type="text" name="side_price" value="{{ old('side_price', $side->side_price) }}">
+    <input type="number" name="inner_cooper_price"
+        value="{{ old('inner_cooper_price', $inner_cooper->inner_cooper_price) }}">
 
     <button type="submit">update</button>
 </form>
