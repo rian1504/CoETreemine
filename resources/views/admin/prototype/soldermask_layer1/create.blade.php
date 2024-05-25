9c307c454de4c87d5e79d0070e9cea65d056ac10
rian1504 <adaadi3526@gmail.com>
2024-05-25 18:14:09 +0700

    Feat: CRUD Prototype Soldermask Layer 1 Membuat CRUD pada Prototype Soldermask Layer 1

--- resources/views/admin/prototype/soldermask_layer1/create.blade.php
+++ resources/views/admin/prototype/soldermask_layer1/create.blade.php
@@ -1,12 +1,10 @@
 <h1>Tambah</h1>
 
-<form action="{{ route('side.store') }}" method="POST">
+<form action="{{ route('soldermask_layer1.store') }}" method="POST">
     @csrf
 
     <label for="">Name</label>
-    <input type="text" name="side_name" value="{{ old('side_name') }}">
-    <label for="">price</label>
-    <input type="number" name="side_price" value="{{ old('side_price') }}">
+    <input type="text" name="soldermask_layer1_name" value="{{ old('soldermask_layer1_name') }}">
 
     <button type="submit">Simpan</button>
 </form>
--- resources/views/admin/prototype/soldermask_layer1/edit.blade.php
+++ resources/views/admin/prototype/soldermask_layer1/edit.blade.php
@@ -1,14 +1,12 @@
 <h1>Edit</h1>
 
-<form action="{{ route('side.update', $side->id_side) }}" method="POST">
+<form action="{{ route('soldermask_layer1.update', $soldermask_layer1->id_soldermask_layer1) }}" method="POST">
     @csrf
     @method('PUT')
 
     <label for="">Name</label>
-    <input type="text" name="side_name" value="{{ old('side_name', $side->side_name) }}">
-
-    <label for="">Price</label>
-    <input type="text" name="side_price" value="{{ old('side_price', $side->side_price) }}">
+    <input type="text" name="soldermask_layer1_name"
+        value="{{ old('soldermask_layer1_name', $soldermask_layer1->soldermask_layer1_name) }}">
 
     <button type="submit">update</button>
 </form>
