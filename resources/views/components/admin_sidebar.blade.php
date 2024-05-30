4ca22efe4346faf94800bea9e0d20670fcb6a3f4
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-05-30 19:50:45 +0700

    Fix dashboard for admin

--- resources/views/components/admin_sidebar.blade.php
+++ resources/views/components/admin_sidebar.blade.php
@@ -39,7 +39,7 @@
             </li>
             <li>
                 <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
-                    <i class="fas fa-file w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
+                    <i class="fas fa-file-lines w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Review File</span>
                     <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-900 dark:text-green-300">3</span>
                 </a>
