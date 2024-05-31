6802744e691ac07e3b8bbfe3c4b8e25ae2c97e9e
alealily <alialily782@gmail.com>
2024-05-31 11:53:24 +0700

    view:assembly side

--- resources/views/admin/assembly/flexible/create.blade.php
+++ resources/views/admin/assembly/flexible/create.blade.php
@@ -5,7 +5,6 @@
 @section('content')
 
 <div>
-    <>
         <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
             <li class="inline-flex items-center">
                 <a href="/admin"
--- resources/views/admin/assembly/side/create.blade.php
+++ resources/views/admin/assembly/side/create.blade.php
@@ -19,7 +19,7 @@
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="m1 9 4-4-4-4" />
                     </svg>
-                    <a href="#"
+                    <a href="/assembly"
                         class="ms-1 text-sm font-medium text-gray-700 hover:text-green-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">Assembly</a>
                 </div>
             </li>
