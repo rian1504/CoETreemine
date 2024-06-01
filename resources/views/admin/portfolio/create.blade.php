35f49bdbb7e484ba92c0ced1b3ed03896c1799b4
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-06-01 14:30:58 +0700

    Fix Sidebar and Add Create Portfolio for Admin

--- resources/views/admin/portfolio/create.blade.php
+++ resources/views/admin/portfolio/create.blade.php
@@ -12,12 +12,68 @@
         <p>></p>
         <a href="/portfolio/create" class="text-admin-900">Create</a>
     </div>
-    <div class=" min-h-screen bg-white rounded-sm px-10 py-6">
-        <div class="flex flex-col my-2">
+    <div class=" min-h-96 bg-white rounded-sm px-10 py-6">
+        <div class="flex flex-col mt-2 mb-6">
             <label for="" class="font-semibold mb-2">Name</label>
-            <input type="text" class="rounded-xl border-admin-900 h-12" placeholder="Input Product Name">
+            <input type="text" class="rounded-lg text-gray-900 border-admin-900 p-4 bg-admin-100 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Input a product name">
+        </div>
+        <div class="flex flex-col mt-2 mb-6">
+            <label for="" class="font-semibold mb-2">Category</label>
+            <select id="" class="rounded-lg text-gray-900 border-admin-900 p-4 bg-admin-100 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
+                <option selected>Choose a portfolio category</option>
+                <option value="US">United States</option>
+                <option value="CA">Canada</option>
+                <option value="FR">France</option>
+                <option value="DE">Germany</option>
+            </select>
+        </div>
+        <div class="flex flex-col mt-2 mb-6">
+            <label for="" class="font-semibold mb-2">Descriptions</label>
+            <textarea type="text" class="rounded-lg text-gray-900 border-admin-900 px-4 pt-6 bg-admin-100 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Input a product descriptions"></textarea>
+        </div>
+        <div class="flex flex-col mt-2 mb-6">
+            <label for="" class="font-semibold mb-2">Specification</label>
+            <input type="text" class="rounded-lg text-gray-900 border-admin-900 p-4 bg-admin-100 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Input a product specification">
+        </div>
+        <div class="flex flex-col mt-2 mb-6">
+            <label for="" class="font-semibold mb-2">Price</label>
+            <input type="text" class="rounded-lg text-gray-900 border-admin-900 p-4 bg-admin-100 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Input a product price">
+        </div>
+        <div class="flex flex-col mt-2 mb-6">
+            <label for="" class="font-semibold mb-2">Image</label>
+            <div class="relative flex items-center">
+                <input type="file" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer" id="fileInput">
+                <input type="text" class="w-full h-14 p-4 border border-admin-900 rounded-l-lg focus:outline-none" placeholder="No file chosen" id="fileText" readonly>
+                <button class="h-14 w-52 bg-admin-900 text-white rounded-r-lg">
+                    Choose File
+            </div>
+        </div>
+        <div>
+            <img class="h-auto max-w-52 rounded-lg shadow-xl dark:shadow-gray-800" id="imagePreview">
+        </div>
+        <div class="grid place-items-end ">
+            <a href="#" class="inline-flex items-center px-5 py-2 text-lg font-medium text-center text-white bg-admin-900 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                Save
+            </a>
         </div>
     </div>
 </div>
 
+<script>
+    document.getElementById('fileInput').addEventListener('change', function() {
+        var fileInput = this;
+        var fileName = fileInput.files[0].name;
+        var fileText = document.getElementById('fileText');
+        var imagePreview = document.getElementById('imagePreview');
+
+        fileText.value = fileName;
+
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+            imagePreview.classList.remove('hidden');
+        }
+        reader.readAsDataURL(fileInput.files[0]);
+    });
+</script>
 @endsection
--- resources/views/components/admin_sidebar.blade.php
+++ resources/views/components/admin_sidebar.blade.php
@@ -5,7 +5,7 @@
         </a>
         <ul class="space-y-2 font-medium">
             <li>
-                <a href="/admin" class="{{ request()->is('admin*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin" class="{{ request()->is('admin') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                         <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
                         <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
@@ -14,19 +14,19 @@
                 </a>
             </li>
             <li>
-                <a href="/admin/category_portfolio" class="{{ request()->is('category_portfolio*') ? 'active' : '' }} flex items-center p-2  rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin/category_portfolio" class="{{ request()->is('admin/category_portfolio*') ? 'active' : '' }} flex items-center p-2  rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <i class="fas fa-th-list text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Portfolio Category</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/portfolio" class="{{ request()->is('portfolio*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin/portfolio" class="{{ request()->is('admin/portfolio*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <i class="fas fa-microchip text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Portfolio</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/prototype" class="{{ request()->is('prototype*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin/prototype" class="{{ request()->is('admin/prototype*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <i class="fas fa-project-diagram text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Prototype</span>
                 </a>
@@ -38,21 +38,21 @@
                 </a>
             </li>
             <li>
-                <a href="/admin/review_file" class="{{ request()->is('review_file*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin/review_file" class="{{ request()->is('admin/review_file*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <i class="fas fa-file-lines text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Review File</span>
                     <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-900 dark:text-green-300">3</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/review_payment" class="{{ request()->is('review_payment*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin/review_payment" class="{{ request()->is('admin/review_payment*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <i class="fas fa-credit-card text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">Review Payment</span>
                     <span class="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-green-800 bg-green-100 rounded-full dark:bg-green-900 dark:text-green-300">3</span>
                 </a>
             </li>
             <li>
-                <a href="/admin/history" class="{{ request()->is('history*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
+                <a href="/admin/history" class="{{ request()->is('admin/history*') ? 'active' : '' }} flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-admin-900 hover:text-white dark:hover:bg-gray-700 group">
                     <i class="fas fa-clock text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white"></i>
                     <span class="flex-1 ms-3 whitespace-nowrap">History</span>
                 </a>
