c1b5f426dbdc6eac22bdb7f103d76484eaac1d44
firmansyah pramudia <firmansyahpramudiaa@gmail.com>
2024-06-10 10:38:38 +0700

    Fix Prototype and Assembly for Admin

--- resources/views/admin/assembly/index.blade.php
+++ resources/views/admin/assembly/index.blade.php
@@ -18,6 +18,9 @@
         <a href="/admin/assembly" class="text-admin-900">Assembly</a>
     </div>
     <div class=" min-h-96 bg-white rounded-sm px-10 py-6">
+        <div class="my-4 flex justify-end">
+            <a href="/admin/assembly/board/create" class=" mt-5 max-w-20 font-semibold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</a>
+        </div>
         <form class=" flex flex-row gap-4 ">
             <label for="countries" class=" inline-flex items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose</label>
             <select id="countries" class=" max-w-40 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
@@ -27,9 +30,6 @@
                 <option value="FR">Assembly side(s)</option>
             </select>
         </form>
-        <div class="my-4">
-            <a href="/admin/assembly/board/create" class=" mt-5 max-w-20 font-semibold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</a>
-        </div>
     </div>
 </div>
 @endsection
--- resources/views/admin/prototype/index.blade.php
+++ resources/views/admin/prototype/index.blade.php
@@ -18,6 +18,9 @@
         <a href="/admin/prototype" class="text-admin-900">Prototype</a>
     </div>
     <div class=" min-h-96 bg-white rounded-sm px-10 py-6">
+        <div class="my-4 flex justify-end">
+            <a href="/admin/prototype/board/create" class=" mt-5 max-w-20 font-semibold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</a>
+        </div>
         <form class=" flex flex-row gap-4">
             <label for="countries" class=" inline-flex items-center mb-2 text-sm font-medium text-gray-900 dark:text-white">Choose</label>
             <select id="countries" class=" max-w-44 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500">
@@ -41,9 +44,6 @@
                 <option value="SL">Silkscreen Layer 1</option>
             </select>
         </form>
-        <div class="my-4">
-            <a href="/admin/prototype/board/create" class=" mt-5 max-w-20 font-semibold text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add</a>
-        </div>
     </div>
 </div>
 @endsection
